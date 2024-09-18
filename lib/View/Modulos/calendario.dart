7628c71f53805d2b4f8d6f4328f67571c93b3efe
jelujr17 <139749706+jelujr17@users.noreply.github.com>
2024-09-18 15:18:41 +0200

    prototipo de calendario

--- lib/View/Modulos/calendario.dart
+++ lib/View/Modulos/calendario.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:kalender/kalender.dart';
 import 'package:smart_family/Model/perfiles.dart';
 import 'package:smart_family/View/navegacion.dart';
 
@@ -17,10 +17,53 @@ class CalendarioScreenState extends State<Calendario> {
   final PageController _pageController = PageController(initialPage: 1);
   late NotchBottomBarController _controller;
 
+  final CalendarController<Event> controller = CalendarController(
+    calendarDateTimeRange: DateTimeRange(
+      start: DateTime(DateTime.now().year - 1),
+      end: DateTime(DateTime.now().year + 1),
+    ),
+  );
+  final CalendarEventsController<Event> eventController =
+      CalendarEventsController<Event>();
+
+  late ViewConfiguration currentConfiguration = viewConfigurations[0];
+  List<ViewConfiguration> viewConfigurations = [
+    CustomMultiDayConfiguration(
+      name: 'Day',
+      numberOfDays: 1,
+      startHour: 6,
+      endHour: 18,
+    ),
+    WeekConfiguration(),
+    MonthConfiguration(),
+    ScheduleConfiguration(),
+  ];
+
   @override
   void initState() {
     super.initState();
     _controller = NotchBottomBarController(index: 1);
+    _addSampleEvents();
+  }
+
+  void _addSampleEvents() {
+    DateTime now = DateTime.now();
+    eventController.addEvents([
+      CalendarEvent(
+        dateTimeRange: DateTimeRange(
+          start: now,
+          end: now.add(const Duration(hours: 1)),
+        ),
+        eventData: Event(title: 'Evento 1'),
+      ),
+      CalendarEvent(
+        dateTimeRange: DateTimeRange(
+          start: now.add(const Duration(hours: 2)),
+          end: now.add(const Duration(hours: 5)),
+        ),
+        eventData: Event(title: 'Evento 2'),
+      ),
+    ]);
   }
 
   @override
@@ -30,39 +73,147 @@ class CalendarioScreenState extends State<Calendario> {
     super.dispose();
   }
 
-  // Definimos la función _onPageChanged
-  void _onPageChanged(int index) {
-    setState(() {
-      _controller.index = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TableCalendar(
-              focusedDay: DateTime.now(),
-              firstDay: DateTime(2000),
-              lastDay: DateTime(2100),
-              calendarFormat: CalendarFormat.month,
-              onDaySelected: (selectedDay, focusedDay) {
-                // Acción al seleccionar un día
-                print('Día seleccionado: $selectedDay');
-              },
-            ),
-          ],
-        ),
+    final calendar = CalendarView<Event>(
+      controller: controller,
+      eventsController: eventController,
+      viewConfiguration: currentConfiguration,
+      tileBuilder: _tileBuilder,
+      multiDayTileBuilder: _multiDayTileBuilder, // Añadido aquí
+      scheduleTileBuilder: _scheduleTileBuilder,
+      components: CalendarComponents(
+        calendarHeaderBuilder: _calendarHeader,
       ),
+      eventHandlers: CalendarEventHandlers(
+        onEventTapped: _onEventTapped,
+        onEventChanged: _onEventChanged,
+        onCreateEvent: _onCreateEvent,
+        onEventCreated: _onEventCreated,
+      ),
+    );
+
+    return Scaffold(
+      body: calendar,
       extendBody: true,
       bottomNavigationBar: CustomBottomNavBar(
         pageController: _pageController,
         controller: _controller,
-        perfil: widget.perfil, 
+        perfil: widget.perfil,
       ),
     );
   }
+
+  CalendarEvent<Event> _onCreateEvent(DateTimeRange dateTimeRange) {
+    return CalendarEvent(
+      dateTimeRange: dateTimeRange,
+      eventData: Event(title: 'Nuevo Evento'),
+    );
+  }
+
+  Future<void> _onEventCreated(CalendarEvent<Event> event) async {
+    eventController.addEvent(event);
+    eventController.deselectEvent();
+  }
+
+  Future<void> _onEventTapped(CalendarEvent<Event> event) async {
+    print('Evento seleccionado: ${event.eventData?.title}');
+  }
+
+  Future<void> _onEventChanged(
+    DateTimeRange initialDateTimeRange,
+    CalendarEvent<Event> event,
+  ) async {
+    eventController.deselectEvent();
+  }
+
+  Widget _tileBuilder(CalendarEvent<Event> event, TileConfiguration configuration) {
+    final color = event.eventData?.color ?? Colors.blue;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      margin: EdgeInsets.zero,
+      elevation: configuration.tileType == TileType.ghost ? 0 : 8,
+      color: configuration.tileType != TileType.ghost
+          ? color
+          : color.withAlpha(100),
+      child: Center(
+        child: configuration.tileType != TileType.ghost
+            ? Text(event.eventData?.title ?? 'Nuevo Evento')
+            : null,
+      ),
+    );
+  }
+
+  Widget _multiDayTileBuilder(CalendarEvent<Event> event, MultiDayTileConfiguration configuration) {
+    final color = event.eventData?.color ?? Colors.blue;
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 2),
+      elevation: configuration.tileType == TileType.selected ? 8 : 0,
+      color: configuration.tileType == TileType.ghost
+          ? color.withAlpha(100)
+          : color,
+      child: Center(
+        child: configuration.tileType != TileType.ghost
+            ? Text(event.eventData?.title ?? 'Nuevo Evento')
+            : null,
+      ),
+    );
+  }
+
+  Widget _scheduleTileBuilder(CalendarEvent<Event> event, DateTime date) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: event.eventData?.color ?? Colors.blue,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(event.eventData?.title ?? 'Nuevo Evento'),
+    );
+  }
+
+  Widget _calendarHeader(DateTimeRange dateTimeRange) {
+    return Row(
+      children: [
+        DropdownMenu(
+          onSelected: (value) {
+            if (value == null) return;
+            setState(() {
+              currentConfiguration = value;
+            });
+          },
+          initialSelection: currentConfiguration,
+          dropdownMenuEntries: viewConfigurations
+              .map((e) => DropdownMenuEntry(value: e, label: e.name))
+              .toList(),
+        ),
+        IconButton(
+          onPressed: controller.animateToPreviousPage,
+          icon: const Icon(Icons.navigate_before_rounded),
+        ),
+        IconButton(
+          onPressed: controller.animateToNextPage,
+          icon: const Icon(Icons.navigate_next_rounded),
+        ),
+        IconButton(
+          onPressed: () {
+            controller.animateToDate(DateTime.now());
+          },
+          icon: const Icon(Icons.today),
+        ),
+      ],
+    );
+  }
+}
+
+class Event {
+  Event({
+    required this.title,
+    this.description,
+    this.color,
+  });
+
+  final String title;
+  final String? description;
+  final Color? color;
 }
