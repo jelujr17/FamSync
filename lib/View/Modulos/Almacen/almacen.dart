8749c81e946ef82741e7c88aced4d9eba860d7a5
Mario <marioml7902@gmail.com>
2025-02-25 12:07:56 +0100

    Convierte la clase Almacen de StatelessWidget a StatefulWidget, implementa la lógica para obtener listas y mejora la interfaz de usuario con textos y elementos visuales actualizados.

--- lib/View/Modulos/Almacen/almacen.dart
+++ lib/View/Modulos/Almacen/almacen.dart
@@ -1,11 +1,41 @@
+import 'package:famsync/Model/Almacen/listas.dart';
 import 'package:famsync/Model/perfiles.dart';
 import 'package:flutter/material.dart';
 import 'package:famsync/Model/categorias.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class Almacen extends StatelessWidget {
+class Almacen extends StatefulWidget {
   const Almacen({super.key, required this.perfil});
   final Perfiles perfil;
+
+  @override
+  AlmacenState createState() => AlmacenState();
+}
+
+class AlmacenState extends State<Almacen> {
+  List<Listas> listas = [];
+  bool isLoading = true;
+  String errorMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    obtenerListas();
+  }
+
+  void obtenerListas() async {
+    try {
+      listas = await ServiciosListas().getListas(widget.perfil.UsuarioId, widget.perfil.Id);
+      setState(() {
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+        errorMessage = 'Error al obtener las listas: $e';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +44,19 @@ class Almacen extends StatelessWidget {
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(vertical: 16),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  "Almacén",
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
               _AlmacenState(),
-              const ListasBanner(),
+              ListasBanner(listas:listas),
               const PopularProducts(),
               const SizedBox(height: 20),
               const RecentlyAddedProducts(),
@@ -42,32 +82,6 @@ class _AlmacenState extends StatelessWidget {
     return luminance > 0.5 ? Colors.black : Colors.white;
   }
 
- 
-
-  /*@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 40),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(
-                  "Almacén",
-                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }*/
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -79,13 +93,14 @@ class _AlmacenState extends StatelessWidget {
           const SizedBox(width: 16),
           IconBtnWithCounter(
             // numOfitem: 3,
-            svgSrc: cartIcon,
+            svgSrc: filtroIcono,
+            numOfitem: 2,
+
             press: () {},
           ),
           const SizedBox(width: 8),
           IconBtnWithCounter(
-            svgSrc: bellIcon,
-            numOfitem: 3,
+            svgSrc: masIcono,
             press: () {},
           ),
         ],
@@ -181,7 +196,7 @@ class BarraBusqueda extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(12)),
             borderSide: BorderSide.none,
           ),
-          hintText: "Search product",
+          hintText: "Buscar producto",
           prefixIcon: const Icon(Icons.search),
         ),
       ),
@@ -190,12 +205,19 @@ class BarraBusqueda extends StatelessWidget {
 }
 
 class ListasBanner extends StatelessWidget {
+  final List<Listas> listas;
+
   const ListasBanner({
     super.key,
+    required this.listas,
   });
 
   @override
   Widget build(BuildContext context) {
+    String titulo = listas.isNotEmpty ? "Tus listas:" : "No tienes listas aún";
+    String contenido =
+        listas.isNotEmpty ?  listas.map((e) => e.Nombre).join(", ") : "¡Crea una nueva!";
+
     return Container(
       width: double.infinity,
       margin: const EdgeInsets.all(20),
@@ -207,15 +229,15 @@ class ListasBanner extends StatelessWidget {
         color: const Color(0xFF4A3298),
         borderRadius: BorderRadius.circular(20),
       ),
-      child: const Text.rich(
+      child: Text.rich(
         TextSpan(
-          style: TextStyle(color: Colors.white),
+          style: const TextStyle(color: Colors.white),
           children: [
-            TextSpan(text: "A Summer Surpise\n"),
+            TextSpan(text: "$titulo\n"),
             TextSpan(
-              text: "Cashback 20%",
-              style: TextStyle(
-                fontSize: 24,
+              text: contenido,
+              style: const TextStyle(
+                fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
@@ -229,12 +251,12 @@ class ListasBanner extends StatelessWidget {
 class SectionTitle extends StatelessWidget {
   const SectionTitle({
     super.key,
-    required this.title,
-    required this.press,
+    required this.titulo,
+    required this.accion,
   });
 
-  final String title;
-  final GestureTapCallback press;
+  final String titulo;
+  final GestureTapCallback accion;
 
   @override
   Widget build(BuildContext context) {
@@ -242,7 +264,7 @@ class SectionTitle extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          title,
+          titulo,
           style: const TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.w600,
@@ -250,9 +272,9 @@ class SectionTitle extends StatelessWidget {
           ),
         ),
         TextButton(
-          onPressed: press,
+          onPressed: accion,
           style: TextButton.styleFrom(foregroundColor: Colors.grey),
-          child: const Text("See more"),
+          child: const Text("Ver más"),
         ),
       ],
     );
@@ -269,8 +291,8 @@ class PopularProducts extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: SectionTitle(
-            title: "Popular Products",
-            press: () {},
+            titulo: "Popular Products",
+            accion: () {},
           ),
         ),
         Padding(
@@ -308,8 +330,8 @@ class RecentlyAddedProducts extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: SectionTitle(
-            title: "Recently Added",
-            press: () {},
+            titulo: "Recently Added",
+            accion: () {},
           ),
         ),
         Padding(
@@ -520,17 +542,23 @@ const heartIcon =
 </svg>
 ''';
 
+const masIcono = '''
+<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12 10V2C12 1.44772 11.5523 1 11 1C10.4477 1 10 1.44772 10 2V10H2C1.44772 10 1 10.4477 1 11C1 11.5523 1.44772 12 2 12H10V20C10 20.5523 10.4477 21 11 21C11.5523 21 12 20.5523 12 20V12H20C20.5523 12 21 11.5523 21 11C21 10.4477 20.5523 10 20 10H12Z" fill="#7C7C7C"/>
+</svg>
+''';
+
 const String description =
     "Wireless Controller for PS4™ gives you what you want in your gaming from over precision control your games to sharing …";
 
-const cartIcon =
+const corritoIcono =
     '''<svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M18.4524 16.6669C18.4524 17.403 17.8608 18 17.1302 18C16.3985 18 15.807 17.403 15.807 16.6669C15.807 15.9308 16.3985 15.3337 17.1302 15.3337C17.8608 15.3337 18.4524 15.9308 18.4524 16.6669ZM11.9556 16.6669C11.9556 17.403 11.3631 18 10.6324 18C9.90181 18 9.30921 17.403 9.30921 16.6669C9.30921 15.9308 9.90181 15.3337 10.6324 15.3337C11.3631 15.3337 11.9556 15.9308 11.9556 16.6669ZM20.7325 5.7508L18.9547 11.0865C18.6413 12.0275 17.7685 12.6591 16.7846 12.6591H10.512C9.53753 12.6591 8.66784 12.0369 8.34923 11.1095L6.30162 5.17154H20.3194C20.4616 5.17154 20.5903 5.23741 20.6733 5.35347C20.7563 5.47058 20.7771 5.61487 20.7325 5.7508ZM21.6831 4.62051C21.3697 4.18031 20.858 3.91682 20.3194 3.91682H5.86885L5.0002 1.40529C4.70961 0.564624 3.92087 0 3.03769 0H0.621652C0.278135 0 0 0.281266 0 0.62736C0 0.974499 0.278135 1.25472 0.621652 1.25472H3.03769C3.39158 1.25472 3.70812 1.48161 3.82435 1.8183L4.83311 4.73657C4.83622 4.74598 4.83934 4.75434 4.84245 4.76375L7.17339 11.5215C7.66531 12.9518 9.00721 13.9138 10.512 13.9138H16.7846C18.304 13.9138 19.6511 12.9383 20.1347 11.4859L21.9135 6.14917C22.0847 5.63369 21.9986 5.06175 21.6831 4.62051Z" fill="#7C7C7C"/>
 </svg>
 ''';
 
-const bellIcon =
-    '''<svg width="15" height="20" viewBox="0 0 15 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M13.9645 15.8912C13.9645 16.1628 13.7495 16.3832 13.4844 16.3832H9.22765H9.21987H1.51477C1.2505 16.3832 1.03633 16.1628 1.03633 15.8912V10.7327C1.03633 7.08053 3.93546 4.10885 7.50043 4.10885C11.0645 4.10885 13.9645 7.08053 13.9645 10.7327V15.8912ZM7.50043 18.9381C6.77414 18.9381 6.18343 18.3327 6.18343 17.5885C6.18343 17.5398 6.18602 17.492 6.19034 17.4442H8.81052C8.81484 17.492 8.81743 17.5398 8.81743 17.5885C8.81743 18.3327 8.22586 18.9381 7.50043 18.9381ZM9.12488 3.2292C9.35805 2.89469 9.49537 2.48673 9.49537 2.04425C9.49537 0.915044 8.6024 0 7.50043 0C6.39847 0 5.5055 0.915044 5.5055 2.04425C5.5055 2.48673 5.64281 2.89469 5.87512 3.2292C2.51828 3.99204 0 7.06549 0 10.7327V15.8912C0 16.7478 0.679659 17.4442 1.51477 17.4442H5.15142C5.14883 17.492 5.1471 17.5398 5.1471 17.5885C5.1471 18.9186 6.20243 20 7.50043 20C8.79843 20 9.8529 18.9186 9.8529 17.5885C9.8529 17.5398 9.85117 17.492 9.84858 17.4442H13.4844C14.3203 17.4442 15 16.7478 15 15.8912V10.7327C15 7.06549 12.4826 3.99204 9.12488 3.2292Z" fill="#626262"/>
+const filtroIcono = '''
+<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M2 4C2 3.44772 2.44772 3 3 3H19C19.5523 3 20 3.44772 20 4C20 4.55228 19.5523 5 19 5H3C2.44772 5 2 4.55228 2 4ZM5 10C5 9.44772 5.44772 9 6 9H16C16.5523 9 17 9.44772 17 10C17 10.5523 16.5523 11 16 11H6C5.44772 11 5 10.5523 5 10ZM9 16C9 15.4477 9.44772 15 10 15H12C12.5523 15 13 15.4477 13 16C13 16.5523 12.5523 17 12 17H10C9.44772 17 9 16.5523 9 16Z" fill="#7C7C7C"/>
 </svg>
 ''';
