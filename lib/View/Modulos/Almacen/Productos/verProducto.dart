76e0f4c6c9f9228240d536170993cd9516239867
Mario <marioml7902@gmail.com>
2025-03-03 12:02:53 +0100

    Actualiza archivos de configuración de huellas digitales para las arquitecturas x86, x86_64 y arm64-v8a

--- lib/View/Modulos/Almacen/Productos/verProducto.dart
+++ lib/View/Modulos/Almacen/Productos/verProducto.dart
@@ -1,7 +1,6 @@
 import 'package:famsync/Model/Almacen/producto.dart';
 import 'package:famsync/Model/perfiles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class DetallesProducto extends StatelessWidget {
   const DetallesProducto({
@@ -15,20 +14,22 @@ class DetallesProducto extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = demoProducts[0];
     return Scaffold(
       extendBody: true,
       extendBodyBehindAppBar: true,
       backgroundColor: const Color(0xFFF5F6F9),
       appBar: PreferredSize(
         preferredSize:
-            const Size.fromHeight(100), // Aumenta la altura del AppBar
+            const Size.fromHeight(120), 
+            // Aumenta la altura del AppBar
         child: AppBar(
+          
           backgroundColor: Colors.transparent,
-          automaticallyImplyLeading: false, // Desactiva el botón por defecto
+          automaticallyImplyLeading: false,
+           // Desactiva el botón por defecto
           flexibleSpace: Padding(
             padding:
-                const EdgeInsets.only(left: 0, top: 120), // Ajusta la posición
+                const EdgeInsets.only(left: 0, top: 100), // Ajusta la posición
             child: Align(
               alignment: Alignment.centerLeft,
               child: ElevatedButton(
@@ -57,15 +58,15 @@ class DetallesProducto extends StatelessWidget {
             color: Colors.white,
             child: Column(
               children: [
-                ProductDescription(
-                  product: product,
+                ProductoCard(
+                  producto: producto,
                   pressOnSeeMore: () {},
                 ),
                 TopRoundedContainer(
                   color: const Color(0xFFF6F7F9),
                   child: Column(
                     children: [
-                      ColorDots(product: product),
+                      InformacionProducto(producto: producto),
                     ],
                   ),
                 ),
@@ -90,7 +91,7 @@ class DetallesProducto extends StatelessWidget {
                 ),
               ),
               onPressed: () {},
-              child: const Text("Add To Cart"),
+              child: const Text("Añadir a una lista"),
             ),
           ),
         ),
@@ -255,14 +256,14 @@ class ImagenPequena extends StatelessWidget {
   }
 }
 
-class ProductDescription extends StatelessWidget {
-  const ProductDescription({
-    Key? key,
-    required this.product,
+class ProductoCard extends StatelessWidget {
+  const ProductoCard({
+    super.key,
+    required this.producto,
     this.pressOnSeeMore,
-  }) : super(key: key);
+  });
 
-  final Product product;
+  final Productos producto;
   final GestureTapCallback? pressOnSeeMore;
 
   @override
@@ -273,33 +274,8 @@ class ProductDescription extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Text(
-            product.title,
+            producto.Nombre,
             style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ),
-        Align(
-          alignment: Alignment.centerRight,
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            width: 48,
-            decoration: BoxDecoration(
-              color: product.isFavourite
-                  ? const Color(0xFFFFE6E6)
-                  : const Color(0xFFF5F6F9),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                bottomLeft: Radius.circular(20),
-              ),
-            ),
-            child: SvgPicture.string(
-              heartIcon,
-              colorFilter: ColorFilter.mode(
-                  product.isFavourite
-                      ? const Color(0xFFFF4848)
-                      : const Color(0xFFDBDEE4),
-                  BlendMode.srcIn),
-              height: 16,
-            ),
           ),
         ),
         Padding(
@@ -308,78 +284,62 @@ class ProductDescription extends StatelessWidget {
             right: 64,
           ),
           child: Text(
-            product.description,
+            "Se encuentra en la tienda: ${producto.Tienda}",
             maxLines: 3,
           ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(
             horizontal: 20,
-            vertical: 12,
+            vertical: 10,
           ),
-          child: GestureDetector(
-            onTap: () {},
-            child: const Row(
-              children: [
-                Text(
-                  "See More Detail",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w600, color: Color(0xFFFF7643)),
-                ),
-                SizedBox(width: 5),
-                Icon(
-                  Icons.arrow_forward_ios,
-                  size: 12,
+          child: Row(
+            children: [
+              const Text("Precio: "),
+              Text(
+                "${producto.Precio}€",
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
                   color: Color(0xFFFF7643),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        )
+        ),
       ],
     );
   }
 }
 
-class ColorDots extends StatelessWidget {
-  const ColorDots({
-    Key? key,
-    required this.product,
-  }) : super(key: key);
+class InformacionProducto extends StatelessWidget {
+  const InformacionProducto({
+    super.key,
+    required this.producto,
+  });
 
-  final Product product;
+  final Productos producto;
 
   @override
   Widget build(BuildContext context) {
-    // Now this is fixed and only for demo
-    int selectedColor = 3;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ...List.generate(
-            product.colors.length,
-            (index) => ColorDot(
-              color: product.colors[index],
-              isSelected: index == selectedColor,
-            ),
-          ),
-          const Spacer(),
-          RoundedIconBtn(
-            icon: Icons.remove,
-            press: () {},
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Text(
-              "1",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ),
-          RoundedIconBtn(
-            icon: Icons.add,
-            showShadow: true,
-            press: () {},
+          const Text("Producto visible para los perfiles:"),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              ...List.generate(
+                producto.Visible.length,
+                (index) => IconoPerfil(
+                  idPerfil: producto.Visible[index],
+                  esCreador : producto.IdPerfilCreador == producto.Visible[index],
+                ),
+              ),
+              const Spacer(),
+            ],
           ),
         ],
       ),
@@ -387,46 +347,64 @@ class ColorDots extends StatelessWidget {
   }
 }
 
-class ColorDot extends StatelessWidget {
-  const ColorDot({
-    Key? key,
-    required this.color,
-    this.isSelected = false,
-  }) : super(key: key);
+class IconoPerfil extends StatelessWidget {
+  const IconoPerfil({
+    super.key,
+    required this.idPerfil,
+    this.esCreador = false,
+  });
 
-  final Color color;
-  final bool isSelected;
+  final int idPerfil;
+  final bool esCreador;
+
+  Future<Widget> loadProfileIcon() async {
+    Perfiles? perfilAux = await ServicioPerfiles().getPerfilById(idPerfil);
+    final imageFile =
+        await ServicioPerfiles().obtenerImagen(perfilAux!.FotoPerfil);
+    return Image.file(imageFile);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 2),
-      padding: const EdgeInsets.all(8),
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        border: Border.all(
-            color: isSelected ? const Color(0xFFFF7643) : Colors.transparent),
-        shape: BoxShape.circle,
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-        ),
-      ),
+    return FutureBuilder<Widget>(
+      future: loadProfileIcon(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return const Icon(Icons.error, color: Colors.red);
+        } else {
+          return Container(
+            margin: const EdgeInsets.only(
+                right: 4), // Ajusta el margen si es necesario
+            padding: const EdgeInsets.all(8),
+            height: 60, // Aumenta el tamaño del contenedor
+            width: 60, // Aumenta el tamaño del contenedor
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              border: Border.all(
+                  color: esCreador
+                      ? const Color(0xFFFF7643)
+                      : Colors.transparent),
+              shape: BoxShape.circle,
+            ),
+            child: ClipOval(
+              child: snapshot.data!,
+            ),
+          );
+        }
+      },
     );
   }
 }
 
 class RoundedIconBtn extends StatelessWidget {
   const RoundedIconBtn({
-    Key? key,
+    super.key,
     required this.icon,
     required this.press,
     this.showShadow = false,
-  }) : super(key: key);
+  });
 
   final IconData icon;
   final GestureTapCancelCallback press;
@@ -462,118 +440,3 @@ class RoundedIconBtn extends StatelessWidget {
     );
   }
 }
-
-class Product {
-  final int id;
-  final String title, description;
-  final List<String> images;
-  final List<Color> colors;
-  final double rating, price;
-  final bool isFavourite, isPopular;
-
-  Product({
-    required this.id,
-    required this.images,
-    required this.colors,
-    this.rating = 0.0,
-    this.isFavourite = false,
-    this.isPopular = false,
-    required this.title,
-    required this.price,
-    required this.description,
-  });
-}
-
-// Our demo Products
-
-List<Product> demoProducts = [
-  Product(
-    id: 1,
-    images: [
-      "https://i.postimg.cc/c19zpJ6f/Image-Popular-Product-1.png",
-      "https://i.postimg.cc/zBLc7fcF/ps4-console-white-2.png",
-      "https://i.postimg.cc/KYpWtTJY/ps4-console-white-3.png",
-      "https://i.postimg.cc/YSCV4RNV/ps4-console-white-4.png"
-    ],
-    colors: [
-      const Color(0xFFF6625E),
-      const Color(0xFF836DB8),
-      const Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Wireless Controller for PS4™",
-    price: 64.99,
-    description: description,
-    rating: 4.8,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 2,
-    images: [
-      "https://i.postimg.cc/CxD6nH74/Image-Popular-Product-2.png",
-    ],
-    colors: [
-      const Color(0xFFF6625E),
-      const Color(0xFF836DB8),
-      const Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Nike Sport White - Man Pant",
-    price: 50.5,
-    description: description,
-    rating: 4.1,
-    isPopular: true,
-  ),
-  Product(
-    id: 3,
-    images: [
-      "https://i.postimg.cc/1XjYwvbv/glap.png",
-    ],
-    colors: [
-      const Color(0xFFF6625E),
-      const Color(0xFF836DB8),
-      const Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Gloves XC Omega - Polygon",
-    price: 36.55,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 4,
-    images: [
-      "https://i.postimg.cc/d1QWXMYW/Image-Popular-Product-3.png",
-    ],
-    colors: [
-      const Color(0xFFF6625E),
-      const Color(0xFF836DB8),
-      const Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Gloves XC Omega - Polygon",
-    price: 36.55,
-    description: description,
-    rating: 4.1,
-    isFavourite: false,
-    isPopular: true,
-  ),
-];
-
-const String description =
-    "Wireless Controller for PS4™ gives you what you want in your gaming from over precision control your games to sharing …";
-
-const starIcon =
-    '''<svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M12.7201 5.50474C12.9813 5.23322 13.0659 4.86077 12.9476 4.50957C12.8292 4.15777 12.5325 3.90514 12.156 3.83313L9.12773 3.25704C9.03883 3.23992 8.96219 3.18621 8.91743 3.11007L7.41279 0.515295C7.22517 0.192424 6.88365 0 6.49983 0C6.116 0 5.7751 0.192424 5.58748 0.515295L4.08284 3.11007C4.03808 3.18621 3.96144 3.23992 3.87192 3.25704L0.844252 3.83313C0.467173 3.90514 0.171028 4.15777 0.0526921 4.50957C-0.0662565 4.86077 0.0189695 5.23322 0.280166 5.50474L2.37832 7.68397C2.43963 7.74831 2.46907 7.83508 2.45803 7.92185L2.09199 10.8725C2.04661 11.2397 2.20419 11.5891 2.51566 11.8063C2.6996 11.935 2.91236 11.9999 3.12696 11.9999C3.27595 11.9999 3.42617 11.9687 3.56842 11.9055L6.36984 10.6577C6.45262 10.6211 6.54704 10.6211 6.62981 10.6577L9.43185 11.9055C9.7795 12.0601 10.1725 12.0235 10.484 11.8063C10.7955 11.5891 10.9537 11.2397 10.9083 10.8725L10.5416 7.92244C10.5306 7.83508 10.56 7.74831 10.6226 7.68397L12.7201 5.50474Z" fill="#FFC416"/>
-</svg>
-''';
-
-const heartIcon =
-    '''<svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M16.5266 8.61383L9.27142 15.8877C9.12207 16.0374 8.87889 16.0374 8.72858 15.8877L1.47343 8.61383C0.523696 7.66069 0 6.39366 0 5.04505C0 3.69644 0.523696 2.42942 1.47343 1.47627C2.45572 0.492411 3.74438 0 5.03399 0C6.3236 0 7.61225 0.492411 8.59454 1.47627C8.81857 1.70088 9.18143 1.70088 9.40641 1.47627C11.3691 -0.491451 14.5629 -0.491451 16.5266 1.47627C17.4763 2.42846 18 3.69548 18 5.04505C18 6.39366 17.4763 7.66165 16.5266 8.61383Z" fill="#DBDEE4"/>
-</svg>
-''';
